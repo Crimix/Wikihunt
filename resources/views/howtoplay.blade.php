9243df3dba3fa22ddfb86fab56ed5e45e1e499a8
Crimix <christofferm@live.dk>
2017-11-02 14:07:24 +0100

    Added lots of stuffs

--- resources/views/howtoplay.blade.php
+++ resources/views/howtoplay.blade.php
@@ -2,20 +2,22 @@
 
 @section('content')
 <div class="container">
-    <div class="content">
-		<div class="title m-b-md">
-				How to Play
-		</div>
+     <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">How to Play</div>
 
-        <div class="box">
-                You can't play the game yet, you dummy!
-				But on a more serious note, the goal of the game is go from 
-				a wikipedia page to specific 'goal' wikipedia page. When 
-				you reach the 'goal' you will be scored according to how 
-				fast you reached the goal page, as well as how few clicks 
-				you used to get it.     
+                <div class="panel-body">
+					You can't play the game yet, you dummy!
+					But on a more serious note, the goal of the game is go from 
+					a wikipedia page to specific 'goal' wikipedia page. When 
+					you reach the 'goal' you will be scored according to how 
+					fast you reached the goal page, as well as how few clicks 
+					you used to get it.     
+				</div>
+            </div>
         </div>
-	</div>
+    </div>
 </div>
 @endsection
 	
--- resources/views/startGame.blade.php
+++ resources/views/startGame.blade.php
@@ -2,10 +2,16 @@
 
 @section('content')
 <div class="container">
-<p> There no page </p>
+     <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Play!</div>
 
-<div id=test>
-<iframe id=iframe src="https://da.wikipedia.org/wiki/Adolf_Hitler?printable=yes" height="600" width="800" onLoad="alert('document.referrer');">
-</div>
+                <div class="panel-body">
+					<iframe id=iframe src="https://da.wikipedia.org/wiki/Adolf_Hitler?printable=yes" height="600" width="800" onLoad="alert('document.referrer');">  
+				</div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
